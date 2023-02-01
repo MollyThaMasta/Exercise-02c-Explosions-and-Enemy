cfc199130b06491f6a4feac12488cca0c21b20ac
MollyThaMasta <mollyapugh@gmail.com>
2023-02-01 08:57:04 -0500

    in progress

--- Player/Bullet.gd
+++ Player/Bullet.gd
@@ -4,6 +4,8 @@ var velocity = Vector2.ZERO
 var speed = 500.0
 var damage = 1
 
+onready var Explosion = load("res://Effects/Explosion.tscn")
+var Effects = null
 
 func _ready():
 	velocity = Vector2(0,-speed).rotated(rotation)
@@ -16,6 +18,11 @@ func _physics_process(_delta):
 
 
 func _on_Area2D_body_entered(body):
+	Effects = get_node_or_null("/root/Game/Effects")
+	if Effects != null:
+		var explosion = Explosion.instance()
+		Effects.add_child(explosion)
+		explosion.global_position = global_position
 	queue_free()
 
 
